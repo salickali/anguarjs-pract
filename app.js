6288ab19837e1395676144341847f1c09a644cf4
salickali <mdsalick.ali@gmail.com>
2020-09-16 13:52:35 +0530

    first commit

--- app.js
+++ app.js
@@ -1,20 +1,79 @@
 (function (){
 'use strict';
+
+
+var l1 = ["salick ali",'ali','salman','aamina'];
+var l2 = [
+
+    {
+        name: 'juice',
+        qty: "2liter"
+    },
+    {
+        name: 'ice',
+        qty: "2liter"
+    },
+    {
+        name: 'water',
+        qty: "2liter"
+    }
+];
     angular.module('LunchCheck',[])
 
-    .controller('LunchCheckController',LunchCheckController);
-    LunchCheckController.$inject = ['$scope']
+    .controller('LunchCheckController',LunchCheckController)
+    .controller('Pctrl',Pctrl)
+    .controller('Cctrl',Cctrl)
+    .filter('rz',zerotrunFilter);
 
-    function LunchCheckController ($scope){
+    Pctrl.$inject = ['$scope'];
+    function Pctrl(){
+        var parent = this;
+        parent.value = 1;
+
+    }
+    Cctrl.$inject = ['$scope']
+    function Cctrl($scope){
+
+        var child = this;
+        child.value = 5;
+    }
+    LunchCheckController.$inject = ['$scope','rzFilter']
+
+    function LunchCheckController ($scope, rzFilter){
         $scope.items = '';
         $scope.checked = false;
-      
+
+        $scope.l1 = l1;
+        $scope.l2 = l2;
+
+        $scope.Add_to_cart = function(){
+          var ad =  { name: $scope.nameitem,
+            qty: $scope.qitem};
+            // alert(ad.name)
+
+           
+               
+            $scope.l2.push(ad);
+           
+        };
+        
+        
+        $scope.original =function(){
+            var msg = 'salick';
+            return msg;
+          };
+        
+      $scope.change =function(){
+        var msg = 'salick';
+        return rzFilter(msg);
+      };
 
 
         $scope.Check = function(){
+            
             if($scope.items.trim().length === 0){
                 $scope.empty = true;
-                $scope.message = 'Please Enter data first';
+                $scope.messagee = 'Please Enter data first';
             }
             else{
                 $scope.checked = true;
@@ -36,6 +95,14 @@
            
         };
 
+    }
+
+    function zerotrunFilter(){
+        return function(input, from ,to){
+           input  = input.replace(from,to);
+            return input;
+
+        }
     }
 
   
